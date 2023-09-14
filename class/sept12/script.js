aa4d1d173046ae983a7f1dd4f511f5c3f7c58fe2
Brooks <bslaski@email.sc.edu>
2023-09-14 00:47:26 -0400

    Adding assignments 3,4,5 for 242

---
+++ class/sept12/script.js
@@ -0,0 +1,19 @@
+const changeText = () => {
+    const helloP = document.getElementById("date");
+    helloP.innerHTML = "hi";
+    helloP.classList.add("special");
+}
+
+const showGhost = () => {
+    document.getElementById("ghost").classList.remove("hide");
+}
+
+const hideGhost = () => {
+    document.getElementById("ghost").classList.add("hide");
+}
+
+
+window.onload = () => {
+    document.getElementById("button-click").onclick = changeText;
+}
+
