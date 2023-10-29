ade571f596f7cb74fe632c02f6ff11e75f484c3e
Brooks <bslaski@email.sc.edu>
2023-10-29 17:23:31 -0400

    assignment11

---
+++ assignments/assignment11/script.js
@@ -0,0 +1,64 @@
+const getMovies = async (url) => {
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showMovies = async () => {
+    const movieSection = document.getElementById("movie");
+
+    const movieData = await getMovies(
+        "https://portiaportia.github.io/json/movies.json"
+    );
+
+    const movies = movieData;
+
+    movies.forEach((movie) => {
+        movieSection.append(getMovieItem(movie));
+    });
+};
+
+const getMovieItem = (movie) => {
+    const section = document.createElement("section");
+
+    const img = document.createElement("img");
+    img.src = movie.img;
+    section.append(img);
+
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("movie-text");
+
+    const name = document.createElement("h1");
+    name.innerHTML = movie.title;
+    section.append(name);
+    
+    const director = document.createElement("p");
+    director.innerHTML = "<strong>Director:</strong> " + movie.director;
+    section.append(director);
+    
+    const actors = document.createElement("p");
+    actors.innerHTML = "<strong>Actors:</strong> " + movie.actors.join(", ");
+    section.append(actors);
+    
+    const year = document.createElement("p");
+    year.innerHTML = "<strong>Year Released:</strong> " + movie.year;
+    section.append(year);
+    
+    const genres = document.createElement("p");
+    genres.innerHTML = "<strong>Genres:</strong> " + movie.genres.join(", ");
+    section.append(genres);
+    
+    const description = document.createElement("p");
+    description.innerHTML = movie.description;
+    section.append(description);    
+
+    section.classList.add("movie-style");
+    return section;
+};
+
+window.onload = () => {
+    showMovies();
+};
